8852fe3223f489cc35e69d2d649b5e3373e93993
Maruf System <irfans0727@gmail.com>
2025-07-12 01:34:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+/**
+ * 💫Butterfly🦋 Sizu💟 Messenger Bot
+ * Owner: Maruf Billah | Credit: Butterfly Sizu💟🦋 & Maruf System💫
+ */
+
 const express = require('express');
 const app = express();
+const config = require('./config');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Maruf Bot is Running!');
+  res.send('💫Butterfly🦋 Sizu💟 Bot is Live! (Owner: Maruf Billah)');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+// Event handler example (message)
+const messageEvent = require('./events/message');
+app.post('/webhook', (req, res) => {
+  messageEvent(req, res);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`💫Butterfly🦋 Sizu💟 is running on port ${PORT}`);
 });
