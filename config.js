a92275fffcf61b8dcaddf58db8340c30c1b1a99c
Maruf System <irfans0727@gmail.com>
2025-07-12 01:34:49 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Config file for 💫Butterfly🦋 Sizu💟 Messenger Bot
+ * Note: DO NOT PUT SECRETS HERE in Public Repo!
+ */
+
+module.exports = {
+  botName: '💫Butterfly🦋 Sizu💟',
+  owner: 'Maruf Billah',
+  credit: 'Butterfly Sizu💟🦋 & Maruf System💫'
+};
