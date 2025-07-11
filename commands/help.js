a77569f2d7c325ff52f0721f2fdd31656d724959
Maruf System <irfans0727@gmail.com>
2025-07-12 01:43:02 +0600

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,6 @@
+/**
+ * Help command for 💫Butterfly🦋 Sizu💟
+ */
+module.exports = function (sender, sendMessage) {
+  sendMessage(sender, "আমি 💫Butterfly🦋 Sizu💟 — আপনার সকল কমান্ড পেতে /help লিখুন!");
+};
